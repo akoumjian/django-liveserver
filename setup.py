c52c80779b8698a0e9631004363c8f8b770f7614
Alec Koumjian <akoumjian@gmail.com>
2012-03-03 12:30:54 -0800

    Fixed packaging.

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
 setup(
-    name = "django-liveserver",
-    version = "0.1a",
-    packages = find_packages(),
+    name="django-liveserver",
+    version="0.1a-2",
+    packages=[
+        'django_liveserver',
+    ],
 
-    install_requires = ['django>=1.3'],
+    install_requires=['django>=1.3'],
 
-    # package_data = {
-    # },
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst', '*.md'],
+    },
 
     # metadata for upload to PyPI
-    author = "Alec Koumjian",
-    author_email = "akoumjian@gmail.com",
-    description = "A backport of the Django 1.4 LiveServerTestCase for 1.3",
-    keywords = "django live server selenium test testcase",
+    author="Alec Koumjian",
+    author_email="akoumjian@gmail.com",
+    description="A backport of the Django 1.4 LiveServerTestCase for 1.3",
+    keywords="django live server selenium test testcase",
+    long_description=open('README.md').read()
 )
